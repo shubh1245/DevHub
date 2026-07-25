935846e8f1960bdb2add284ec931a3ea09c15e5f
Shubham Kumar <shubhamyaduvanshi5421@gmail.com>
2026-07-25 15:47:36 +0530

    Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const ConnectionRequest= require("../models/ConnectionRequest");
-
+const USER_SAFE_DATA = "firstName lastName age gender about skills"
+const User = ("../models/user");
 //get all the pending connection request
 
 userRouter.get("/user/requests/received", userAuth, async(req,res)=>{
@@ -24,6 +25,63 @@ catch(err){
 }
 })
 
-userRouter.get("/user/requests/")
+
+userRouter.get("/user/connections",userAuth,async(req,res)=>{
+  try{
+  const loggedInUser = req.user;
+  const connectionRequests = await ConnectionRequest.find({
+   $or : [
+    {toUserId : loggedInUser._id,status : "accepted"},
+    {fromUserId  : loggedInUser._id,status : "accepted"},
+    ]
+  }).populate("fromUserId",USER_SAFE_DATA)
+  .populate("toUserId",USER_SAFE_DATA);
+
+  const data = connectionRequests.map((row)=>{
+    if(row.fromUserId._id.toString()===loggedInUser._id){
+      return row.toUserId;
+    }
+    return row.fromUserId;
+  });
+  
+  res.json({data});
+}
+catch(err){
+  res.status(400).send({message : err.message});
+}
+});
+
+userRouter.get("/feed",userAuth,async(req,res)=>{
+  try{
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+
+    limit = limit > 50 ? 5: limit;
+
+    const skip = (page - 1) * limit;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or : [{fromUserId : loggedInUser._id},{toUserId : loggedInUser._id}],
+    }).select("fromUserId toUserId").skip(skip).limit(limit);
+
+    const hideUsersFromFeed=new Set();
+    connectionRequests.forEach((req)=>{
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and : [
+         {_id : { $nin  : Array.from()}},
+         {_id : { $ne  : loggedInUser._id} },
+      ]
+    }).select(USER_SAFE_DATA);
+
+      res.send(users);
+  } catch(err){
+    res.status(404).send({message : err.message});
+  }
+});
 
 module.exports = userRouter;
